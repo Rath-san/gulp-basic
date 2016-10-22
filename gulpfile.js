f5809ea01c8ece75ca87ca9516ade6f7f90b1864
Raths-san <rim.kozlowski@gmail.com>
2016-10-22 21:14:47 +0200

    remodelinf workflow p2

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,33 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+var sass        = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var notify = require ('gulp-notify');
 // Static Server + watching sass/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./"
+        server: "src/"
     });
-
-    gulp.watch("./sass/*.sass", ['sass']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
-    gulp.watch("./js/*.js").on('change', browserSync.reload);
+    // Watch for changes in sass, html, js
+    gulp.watch("src/sass/*.sass", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/js/*.js").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("./sass/main.sass")
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
+    return gulp.src("src/sass/*.sass")
+        .pipe(sass()).on('error', function(err) {
+            return notify().write(err);
+        })
+         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest("./css"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream())
+        // .pipe(notify({ message: 'Inject complete' }));
 });
 
 gulp.task('default', ['serve']);
-
-
-// DEPLOYMENT
